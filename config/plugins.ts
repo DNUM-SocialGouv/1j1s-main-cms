73fd486b76a6c7bc19984760327be44c99b52da7
Simon Belbeoch <simon.belbeoch@ln1.eu>
2022-12-09 18:14:55 +0100

    feat(recherche) Ajout d'un slug pour la recherche et de la transformation des entry

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,6 +1,24 @@
 const LIMITE_ENTRIES_EVENEMENT = 1000
+const LIMITE_MAX_LOGEMENTS = 100000
 const LIMITE_MAX_EVENEMENTS = 100000
 const LIMITE_MAX_FACETS = 100000
+
+const getSurfaceToDisplay = (surface: string, surfaceMax: string): string => {
+  if (surfaceMax === null) return `${surface}m²`
+  return `de ${surface} à ${surfaceMax}m²`
+}
+interface Localisation  {
+  ville: string
+  codePostal: string
+}
+
+const getLocalisationToDisplay = (localisation: Localisation): string => {
+  const { ville, codePostal } = localisation
+  if (ville && codePostal) return codePostal + ' - ' + ville;
+  if (codePostal) return codePostal
+  return ville;
+}
+
 
 module.exports = ({env}) => ({
   meilisearch: {
@@ -25,6 +43,57 @@ module.exports = ({env}) => ({
         },
       }
     },
+
+    "annonce-de-logement": {
+      entriesQuery: {
+        limit: 5000,
+      },
+      settings: {
+        filterableAttributes: [
+          "typeBien",
+          "type",
+          "localisation.ville",
+          "localisation.codePostal",
+        ],
+        searchableAttributes: [
+          "localisation.ville",
+          "localisation.codePostal",
+        ],
+        displayedAttributes: [
+          "slug",
+          "titre",
+          "dateDeDisponibilite",
+          "dateDeMiseAJour",
+          "prix",
+          "prixHT",
+          "surface",
+          "surfaceMax",
+          "surfaceAAfficher",
+          "type",
+          "url",
+          "sourceUpdatedAt",
+          "localisationAAfficher",
+        ],
+        pagination: {
+          maxTotalHits: LIMITE_MAX_LOGEMENTS
+        },
+        faceting: {
+          maxValuesPerFacet: LIMITE_MAX_FACETS
+        },
+      },
+      transformEntry({ entry }) {
+        return {
+          ...entry,
+          surfaceAAfficher: getSurfaceToDisplay(entry.surface, entry.surfaceMax),
+          dateDeMiseAJour: entry.sourceUpdatedAt,
+          localisationAAfficher: getLocalisationToDisplay(entry.localisation),
+          localisation: entry.localisation && {
+            ville: entry.localisation.ville,
+            codePostal: entry.localisation.codePostal,
+          },
+        }
+      },
+    },
   },
   upload: {
     config: {
@@ -38,6 +107,22 @@ module.exports = ({env}) => ({
           Bucket: env('MINIO_BUCKET'),
         },
       },
+    },
+  },
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        'annonce-de-logement': {
+          field: 'slug',
+          references: ['titre', 'identifiantSource']
+        },
+        'evenement': {
+          field: 'slug',
+          references: ['titre', 'idSource']
+        },
+      },
+      slugifyWithCount: true, // DEVNOTE : the count does not work with compound references
     },
   },
 })
