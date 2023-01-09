388dfd66d6ddec67ff95590e139a761c6c8d44dd
SEFR <sebastien.francois@octo.com>
2023-01-09 15:44:02 +0100

    chore(plugins): ajout des plugins sentry & import-export
    Ajout des plugins Sentry & Import-Export
    
    Correction des droits config-sync

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,9 +1,17 @@
-import slugifyConfiguration from "./slugify";
+import configSync from "./config-sync";
+import importExportConfiguration from "./import-export";
 import meilisearchConfiguration from "./meilisearch";
 import minioConfiguration from "./minio";
+import sentryConfiguration from "./sentry";
+import slugifyConfiguration from "./slugify";
+import usersPermissionsConfiguration from "./users-permissions";
 
 export default ({ env }) => ({
+  "config-sync": configSync,
+  "import-export-entries": importExportConfiguration,
   meilisearch: meilisearchConfiguration(env),
+  sentry: sentryConfiguration(env),
   slugify: slugifyConfiguration,
   upload: minioConfiguration(env),
+  "users-permissions": usersPermissionsConfiguration,
 });
