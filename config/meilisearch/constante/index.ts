f250c42995e37045e88866b896c45831616a40d4
SEFR <sebastien.francois@octo.com>
2023-02-06 15:35:31 +0100

    fix(meilisearch): corriger la taille des batch
    Réduire la taille des batchs pour Meilisearch pour limiter l'impact sur l'erreur d'indexation

--- config/meilisearch/constante/index.ts
+++ config/meilisearch/constante/index.ts
@@ -2,5 +2,5 @@
 export default class Constante {
   public static readonly LIMITE_MAX_FACETS = 100000;
   public static readonly LIMITE_MAX_HITS= 100000;
-  public static readonly LIMITE_ENTRIES_QUERY = 5000;
+  public static readonly LIMITE_ENTRIES_QUERY = process.env.MEILISEARCH_BATCH_SIZE;
 }
