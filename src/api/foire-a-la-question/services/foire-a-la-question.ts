d4bf840567554c9d3a89cd97a4c90786459346a6
Olivia Peny-Coblentz <oliv@octo.com>
2023-03-06 16:06:41 +0100

    fix(faq): recreation content type faq

---
+++ src/api/foire-a-la-question/services/foire-a-la-question.ts
@@ -0,0 +1,7 @@
+/**
+ * foire-a-la-question service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::foire-a-la-question.foire-a-la-question');
