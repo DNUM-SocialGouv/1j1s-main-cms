99254bba4f340a9446b7eb591781416191758e20
Dorian De Rosa <dorian.de.rosa@octo.com>
2023-06-26 17:24:39 +0200

    feat(formation): creer la collection de fiches formations

---
+++ src/api/formation-initiale-detail/routes/formation-initiale-detail.ts
@@ -0,0 +1,7 @@
+/**
+ * formation-initiale-detail router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::formation-initiale-detail.formation-initiale-detail');
