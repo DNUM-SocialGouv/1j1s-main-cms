11a6f359444a79a8384583453f185d65d1905379
guillaume.moizan <guillaume.moizan@amac02df473md6r.lan>
2023-09-05 09:29:52 +0200

    chore: suppression de la collection contacts POE

--- src/api/contact-poe/controllers/contact-poe.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * contact-poe controller
- */
-
-import { factories } from "@strapi/strapi";
-
-export default factories.createCoreController('api::contact-poe.contact-poe');
--- src/api/contact-poe/services/contact-poe.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * contact-poe service
- */
-
-import { factories } from "@strapi/strapi";
-
-export default factories.createCoreService('api::contact-poe.contact-poe');
