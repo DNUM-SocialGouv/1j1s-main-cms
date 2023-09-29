7fd0f40a102d3bcee675218ef451203fad62306f
Joseph Page <josephpage@users.noreply.github.com>
2023-09-29 17:35:40 +0200

    fix(ci): heap size issue and revert to L-size Scalingo container

--- terraform/app.tf
+++ terraform/app.tf
@@ -8,7 +8,7 @@ module "main_cms_app" {
 
   containers = {
     web = {
-      size   = terraform.workspace == "production" ? "XL" : "XL"
+      size   = terraform.workspace == "production" ? "L" : "L"
       amount = terraform.workspace == "production" ? 2 : 1
       autoscaler = terraform.workspace == "production" ? {
         min_containers = 2
