70121b0f42aecdf451bb3f697eb1a518359d99b3
Suxue LI <suxue.li@octo.com>
2023-12-05 14:26:10 +0100

    tech(docker): mise à jour du script populate pour continuer à récupérer les données de recette

--- populate-with-recette-data.sh
+++ populate-with-recette-data.sh
@@ -2,7 +2,7 @@
 export SCALINGO_APP='1j1s-main-cms'
 export SCALINGO_REGION='osc-fr1'
 export ADDON_NAME='postgresql'
-export SCALINGO_DB_USER='aj1s_main_c_678'
+export DATABASE_URL='postgres://database-user:database-password@1j1s-main-content-manager-db:5432/cms-principal'
 
 if [ -f .env ]
 then
@@ -13,16 +13,16 @@ fi
 
 scalingo login --api-token ${SCALINGO_API_TOKEN}
 
-addon_id=$(scalingo addons | grep $ADDON_NAME | cut -d'|' -f3 | tr -d ' ')
+addon_id=$(scalingo --app $SCALINGO_APP addons | grep $ADDON_NAME | cut -d'|' -f3 | tr -d ' ')
 mkdir -p tmp && cd tmp
 scalingo --addon ${addon_id} backups-download --output ./backup
-tar -xvf ./backup
+tar -xvf backup
 for filename in *.pgsql; do eval mv \"$filename\" \"backup.pgsql\"; done
 cd ..
 
 docker compose down -v
 docker compose --env-file .env.docker up -d db
 sleep 5
-docker compose exec db psql ${DATABASE_URL} -c "CREATE USER ${SCALINGO_DB_USER} SUPERUSER;"
 docker compose cp ./tmp/backup.pgsql db:/tmp/backup.pgsql
-docker compose exec db pg_restore -d "${DATABASE_URL}" /tmp/backup.pgsql
+docker compose exec db pg_restore --no-privileges --no-owner -d "${DATABASE_URL}" /tmp/backup.pgsql
+rm tmp/backup tmp/backup.pgsql
