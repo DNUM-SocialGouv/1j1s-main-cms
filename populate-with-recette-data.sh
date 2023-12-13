9a79a0393d35949e6518f6ccb65d9e2c419113e0
suli-octo <129731991+suli-octo@users.noreply.github.com>
2023-12-13 10:03:16 +0100

    tech(docker): mise à jour du script populate pour continuer à récupér… (#251)

--- populate-with-recette-data.sh
+++ populate-with-recette-data.sh
@@ -2,7 +2,7 @@
 export SCALINGO_APP='1j1s-main-cms'
 export SCALINGO_REGION='osc-fr1'
 export ADDON_NAME='postgresql'
-export SCALINGO_DB_USER='aj1s_main_c_678'
+export DATABASE_URL='postgres://database-user:database-password@1j1s-main-content-manager-db:5432/cms-principal'
 
 if [ -f .env ]
 then
@@ -23,6 +23,6 @@ cd ..
 docker compose down -v
 docker compose --env-file .env.docker up -d db
 sleep 5
-docker compose exec db psql ${DATABASE_URL} -c "CREATE USER ${SCALINGO_DB_USER} SUPERUSER;"
 docker compose cp ./tmp/backup.pgsql db:/tmp/backup.pgsql
-docker compose exec db pg_restore -d "${DATABASE_URL}" /tmp/backup.pgsql
+docker compose exec db pg_restore --no-privileges --no-owner -d "${DATABASE_URL}" /tmp/backup.pgsql
+rm tmp/backup tmp/backup.pgsql
