5a9859b11a610d6cbebd4f05c666d6f40f142077
Suxue LI <suxue.li@octo.com>
2023-12-13 10:04:58 +0100

    fix: tentative de déposer média dans minio local

--- config/minio/index.ts
+++ config/minio/index.ts
@@ -1,14 +1,19 @@
 export default (env: any) => ({
   config: {
-    provider: "aws-s3",
-    providerOptions: {
-      accessKeyId: env("MINIO_ACCESS_KEY"),
-      secretAccessKey: env("MINIO_SECRET_KEY"),
-      endpoint: env("MINIO_ENDPOINT"),
-      s3ForcePathStyle: true,
-      params: {
-        Bucket: env("MINIO_BUCKET"),
+    provider: 'aws-s3',
+    providerOptions: { // deprecated ? pour s3Options ?
+      s3Options: {
+        accessKeyId: env('MINIO_ACCESS_KEY'),
+        secretAccessKey: env('MINIO_SECRET_KEY'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('MINIO_BUCKET'),
+        },
+        endpoint: env('MINIO_ENDPOINT'),
+        baseUrl: 'https://s3.eu-west-3.amazonaws.com/strapi-medias', // This line sets the custom url format
+        s3ForcePathStyle: true,
       },
+      // baseUrl: 'https://s3.us-east-1.amazonaws.com/main-strapi-medias', // This line sets the custom url format
     },
   },
 });
