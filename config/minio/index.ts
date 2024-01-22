c58976e4f2a0e592cefc1fc5b132a97a54da0337
Guillaume Moizan <guillaume.moizan@octo.com>
2024-01-22 17:16:03 +0100

    feat: test minio upload

--- config/minio/index.ts
+++ config/minio/index.ts
@@ -1,14 +1,25 @@
 export default (env: any) => ({
   config: {
+    provider: 'strapi-provider-upload-ts-minio',
+    providerOptions: {
+      accessKey: env('MINIO_ACCESS_KEY'),
+      secretKey: env('MINIO_SECRET_KEY'),
+      bucket: env('MINIO_BUCKET'),
+      endPoint: env('MINIO_ENDPOINT'),
+    },
+  },
+ /* config: {
     provider: "aws-s3",
     providerOptions: {
       accessKeyId: env("MINIO_ACCESS_KEY"),
       secretAccessKey: env("MINIO_SECRET_KEY"),
       endpoint: env("MINIO_ENDPOINT"),
+      region: 'us-east-1',
+      baseUrl: `${env("MINIO_ENDPOINT")}/${env("MINIO_BUCKET")}`, // This line sets the custom url format
       s3ForcePathStyle: true,
       params: {
         Bucket: env("MINIO_BUCKET"),
       },
     },
-  },
+  }, */
 });
