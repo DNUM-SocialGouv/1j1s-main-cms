1acd91110478787f5fedfe381fb24f3097ac9ec9
Joseph Page <josephpage@users.noreply.github.com>
2024-03-07 17:10:32 +0100

    chore: attempt to fix memory issue with config-sync

--- config/config-sync/index.ts
+++ config/config-sync/index.ts
@@ -1,12 +1,17 @@
 import coreStoresConfigFilesToExclude from "./core-stores-config-files-to-exclude";
 
-export default {
-  enabled: true,
-  config: {
-    syncDir: "config/config-sync/files/",
-    minify: false,
-    importOnBootstrap: true,
-    excludedTypes: ["i18n-locale"],
-    excludedConfig: [...coreStoresConfigFilesToExclude]
-  },
-};
+export default function ({ env }: any) {
+
+  const importOnBootstrap = env.bool('IMPORT_CONFIG_ON_BOOTSTRAP', true) as boolean
+
+  return {
+    enabled: true,
+    config: {
+      syncDir: "config/config-sync/files/",
+      minify: false,
+      importOnBootstrap,
+      excludedTypes: ["i18n-locale"],
+      excludedConfig: [...coreStoresConfigFilesToExclude]
+    }
+  }
+}
