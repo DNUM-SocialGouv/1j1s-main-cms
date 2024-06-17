c6501484fabe94233bbe613d3fb3c13fe8d306a4
julie BRUNETTO <julie.brunetto@octo.com>
2024-06-17 11:19:09 +0200

    feat(banniere): ajout de la banniere de campagne

---
+++ src/api/banniere-campagne/controllers/banniere-campagne.ts
@@ -0,0 +1,7 @@
+/**
+ * banniere-campagne controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::banniere-campagne.banniere-campagne');
---
+++ src/api/banniere-campagne/services/banniere-campagne.ts
@@ -0,0 +1,7 @@
+/**
+ * banniere-campagne service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::banniere-campagne.banniere-campagne');
