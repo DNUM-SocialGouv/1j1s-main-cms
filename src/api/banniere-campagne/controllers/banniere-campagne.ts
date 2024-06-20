f3129bcb0650a2ba529518795aa1a476e49f8a42
julie BRUNETTO <julie.brunetto@octo.com>
2024-06-20 16:50:49 +0200

    feat(banniere): supprimer la campagne

--- src/api/banniere-campagne/controllers/banniere-campagne.ts
+++
@@ -1,7 +0,0 @@
-/**
- * banniere-campagne controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::banniere-campagne.banniere-campagne');
--- src/api/banniere-campagne/services/banniere-campagne.ts
+++
@@ -1,7 +0,0 @@
-/**
- * banniere-campagne service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::banniere-campagne.banniere-campagne');
