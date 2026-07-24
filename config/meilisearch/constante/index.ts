bef7578df7cfc09faedcc846f525e10843291b48
Thibaut <thibaut.poullain@ydrazil.fr>
2026-07-24 12:07:10 +0200

    fix(recherche): abaisse maxTotalHits et maxValuesPerFacet pour limiter le coût unitaire Meilisearch

--- config/meilisearch/constante/index.ts
+++ config/meilisearch/constante/index.ts
@@ -1,6 +1,6 @@
 
 export default class Constante {
-  public static readonly LIMITE_MAX_FACETS = 100000;
-  public static readonly LIMITE_MAX_HITS= 100000;
+  public static readonly LIMITE_MAX_FACETS = 200;
+  public static readonly LIMITE_MAX_HITS= 2000;
   public static readonly LIMITE_ENTRIES_QUERY = process.env.MEILISEARCH_BATCH_SIZE;
 }
